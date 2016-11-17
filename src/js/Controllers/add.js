534aba722397e3367e5fac2d31de996b43d9573c
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-17 15:07:08 -0500

    minor tweaks

--- src/js/Controllers/add.js
+++ src/js/Controllers/add.js
@@ -5,10 +5,9 @@ function AddController ($scope, $http, $state) {
   $scope.addImage = function (image) {
     $http.post(SERVER, image).then(function (response) {
       $state.go('home');
-    })
-  }
-
-
+    });
+  };
+  
 };
 
 AddController.$inject = ['$scope', '$http', '$state']
