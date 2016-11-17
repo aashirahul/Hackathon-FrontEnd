666bdfbc8d98dca42dba46c55329d3bacf0e2709
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-17 14:58:59 -0500

    completed Home Controller, but still don't have access to backend

--- src/js/Controllers/home.js
+++ src/js/Controllers/home.js
@@ -1,8 +1,15 @@
 import { SERVER } from '../server';
 
 function HomeController ($scope, $http) {
+  $scope.images = []
 
+  function init () {
+    $http.get(SERVER).then(function(response) {
+      $scope.images = response.data
+    })
+  };
 
+  init();
 };
 
 HomeController.$inject = ['$scope', '$http']
