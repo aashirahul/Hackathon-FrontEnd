9c67e12a40f718528ec3be13e6f0e34851b1b5ca
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-17 15:12:11 -0500

    completed details controller

--- src/js/Controllers/details.js
+++ src/js/Controllers/details.js
@@ -1,7 +1,16 @@
 import { SERVER } from '../server';
 
 function DetailsController ($scope, $http, $stateParams) {
+  $scope.image = {};
 
+  function init () {
+    let url = SERVER + $stateParams.id;
+    $http.get(url).then(function(response) {
+      $scope.image = response.data;
+    });
+  };
+
+  init();
 
 };
 
