adf242641d961b0b5e6c7509161afdc7e49814ac
Liliya Lantukh <liliya.lantukh@gmail.com>
2016-11-18 10:55:00 -0500

    updated routes to match backend

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 import angular from 'angular';
 
 import { routerConfig } from './routes';
-import { HomeController } from './controllers/home';
+// import { HomeController } from './controllers/home';
 import { AddController } from './controllers/add';
-import { DetailsController } from './controllers/details';
+// import { DetailsController } from './controllers/details';
 
 import 'angular-ui-router';
 
 angular
   .module('app', ['ui.router'])
   .config(routerConfig)
-  .controller('HomeController', HomeController)
+  // .controller('HomeController', HomeController)
   .controller('AddController', AddController)
-  .controller('DetailsController', DetailsController);
+  // .controller('DetailsController', DetailsController);
--- src/js/server.js
+++ src/js/server.js
@@ -1,3 +1,24 @@
-const SERVER = 'https://afternoon-temple-45418.herokuapp.com/';
+const homeSERVER = 'https://afternoon-temple-45418.herokuapp.com/images';
+const addSERVER = 'https://afternoon-temple-45418.herokuapp.com/images/create';
+const detailsSERVER = 'https://afternoon-temple-45418.herokuapp.com/images/:imageId';
 
-export { SERVER };
+export { homeSERVER };
+export { addSERVER };
+export { detailsSERVER };
+
+
+
+
+
+// Route.get('/images','ImageController.read')
+// Route.post('/images/create','ImageController.add')
+// Route.get('/images/:imageId','ImageController.readOne')
+//
+// Route.put('/images/like/:imageId','ImageController.addLike')
+// Route.put('/images/edit/:imageId/','ImageController.edit')
+//
+// Route.delete('images/remove/:imageId','ImageController.remove')
+//
+//
+// Route.get('/images/:imageId/comments','CommentController.read')
+// Route.post('/images/:imageId/comments/create','CommentController.add')
