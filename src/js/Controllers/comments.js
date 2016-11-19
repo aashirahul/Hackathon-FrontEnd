87beec1062c20aae9783aebeb2230c6b334e33db
Ashima Shrivastava <ashima206@gmail.com>
2016-11-19 12:39:49 -0500

    comments are working

--- src/js/Controllers/comments.js
+++ src/js/Controllers/comments.js
@@ -1,15 +1,8 @@
-import { commentSERVER } from '../server';
 
 function CommentController ($scope, $http) {
   $scope.comments = []
 
-  $scope.addComment = function (comment) {
-    $http.post(commentSERVER, comment).then(function (response) {
-      let comment = resp.data;
-      $scope.comments.unshift(comment);
-    });
-    $scope.comments = [];
-  };
+  
 
 };
 
--- src/js/server.js
+++ src/js/server.js
@@ -2,11 +2,16 @@ const homeSERVER = 'https://afternoon-temple-45418.herokuapp.com/images';
 const addSERVER = 'https://afternoon-temple-45418.herokuapp.com/images/create';
 const detailsSERVER = 'https://afternoon-temple-45418.herokuapp.com/images/';
 const likeSERVER = 'https://afternoon-temple-45418.herokuapp.com/images/like/';
+const commentSERVER = 'https://afternoon-temple-45418.herokuapp.com/images/comments/'
+const commentAddSERVER = 'https://afternoon-temple-45418.herokuapp.com/images/comments/create/'
 
 export { homeSERVER };
 export { addSERVER };
 export { detailsSERVER };
 export { likeSERVER };
+export { commentSERVER };
+export { commentAddSERVER };
+
 
 
 
@@ -22,5 +27,5 @@ export { likeSERVER };
 // Route.delete('images/remove/:imageId','ImageController.remove')
 //
 //
-// Route.get('/images/:imageId/comments','CommentController.read')
-// Route.post('/images/:imageId/comments/create','CommentController.add')
+// Route.get('/images/comments/:imageId','CommentController.read')
+// Route.post('/images/comments/create/:imageId','CommentController.add')
